be579cfdd70086f11fb8499910ccddfe2d5ba69c
Michael Nowotny <nowotnym@gmail.com>
2020-06-01 15:46:55 -0700

    - added functionality to convert back from arviz samples dictionary and arviz inference data object to pyjags samples dictionary

--- pyjags/arviz.py
+++ pyjags/arviz.py
@@ -15,22 +15,26 @@ import numpy as np
 import typing as tp
 
 
-def convert_pyjags_samples_dict_to_arviz_inference_data(
-        samples: tp.Dict[str, np.ndarray]) -> az.InferenceData:
+def convert_pyjags_sample_dictionary_to_arviz_sample_dictionary(
+        samples: tp.Dict[str, np.ndarray]) \
+        -> tp.Dict[str, np.ndarray]:
     """
     This function takes a python dictionary of samples that has been generated
-    by sample method of a model instance and returns an Arviz inference data
-    object.
+    by the sample method of a model instance and returns a dictionary of samples
+    in ArviZ format.
+
     Parameters
     ----------
-    samples: a dictionary mapping variable names to Numpy arrays with shape
+    samples: a dictionary mapping variable names to P arrays with shape
              (parameter_dimension, chain_length, number_of_chains)
 
     Returns
     -------
-    An Arviz inference data object
+    a dictionary mapping variable names to NumPy arrays with shape
+             (number_of_chains, chain_length, parameter_dimension)
     """
-    # pyjags returns a dictionary of numpy arrays with shape
+
+    # pyjags returns a dictionary of NumPy arrays with shape
     #         (parameter_dimension, chain_length, number_of_chains)
     # but arviz expects samples with shape
     #         (number_of_chains, chain_length, parameter_dimension)
@@ -47,4 +51,99 @@ def convert_pyjags_samples_dict_to_arviz_inference_data(
                 parameter_name_to_samples_map[parameter_name] = \
                     np.swapaxes(chains, 0, 2)
 
-    return az.convert_to_inference_data(parameter_name_to_samples_map)
+    return parameter_name_to_samples_map
+
+
+def convert_pyjags_samples_dict_to_arviz_inference_data(
+        samples: tp.Dict[str, np.ndarray]) -> az.InferenceData:
+    """
+    This function takes a python dictionary of samples that has been generated
+    by the sample method of a model instance and returns an Arviz inference data
+    object.
+    Parameters
+    ----------
+    samples: a dictionary mapping variable names to NumPy arrays with shape
+             (parameter_dimension, chain_length, number_of_chains)
+
+    Returns
+    -------
+    An Arviz inference data object
+    """
+
+    # pyjags returns a dictionary of NumPy arrays with shape
+    #         (parameter_dimension, chain_length, number_of_chains)
+    # but arviz expects samples with shape
+    #         (number_of_chains, chain_length, parameter_dimension)
+
+    return az.convert_to_inference_data(
+                convert_pyjags_sample_dictionary_to_arviz_sample_dictionary(
+                    samples))
+
+
+def extract_samples_dictionary_from_arviz_inference_data(idata) \
+        -> tp.Dict[str, np.ndarray]:
+    """
+    This function extracts a dictionary mapping parameter names to NumPy arrays
+    of samples with shape (number_of_chains, chain_length, parameter_dimension)
+    from an ArviZ inference data object.
+
+    Parameters
+    ----------
+    idata: An Arviz inference data object
+
+    Returns
+    -------
+    a dictionary mapping variable names to NumPy arrays with shape
+             (number_of_chains, chain_length, parameter_dimension)
+
+    """
+    parameter_name_to_samples_map = {}
+
+    for key, value in idata.posterior.to_dict()['data_vars'].items():
+        parameter_name_to_samples_map[key] = np.array(value['data'])
+
+    return parameter_name_to_samples_map
+
+
+def convert_arviz_sample_dictionary_to_pyjags_sample_dictionary(
+        samples: tp.Dict[str, np.ndarray]) \
+        -> tp.Dict[str, np.ndarray]:
+    """
+    This function takes a python dictionary of samples in ArviZ format
+    and returns the samples as a dictionary in PyJAGS format.
+
+    Parameters
+    ----------
+    samples: a dictionary mapping variable names to NumPy arrays with shape
+             (number_of_chains, chain_length, parameter_dimension)
+
+    Returns
+    -------
+    a dictionary mapping variable names to NumPy arrays with shape
+             (parameter_dimension, chain_length, number_of_chains)
+
+    """
+
+    # pyjags returns a dictionary of NumPy arrays with shape
+    #         (parameter_dimension, chain_length, number_of_chains)
+    # but arviz expects samples with shape
+    #         (number_of_chains, chain_length, parameter_dimension)
+
+    parameter_name_to_samples_map = {}
+
+    for parameter_name, chains in samples.items():
+        if chains.ndim == 2:
+            number_of_chains, chain_length = chains.shape
+            chains = chains.reshape((number_of_chains, chain_length, 1))
+
+        number_of_chains, chain_length, parameter_dimension \
+            = chains.shape
+
+        parameter_name_to_samples_map[parameter_name] = \
+            np.swapaxes(chains, 0, 2)
+
+    return parameter_name_to_samples_map
+
+
+def from_pyjags(posterior: tp.Dict[str, np.ndarray]) -> az.InferenceData:
+    return convert_pyjags_samples_dict_to_arviz_inference_data(posterior)
