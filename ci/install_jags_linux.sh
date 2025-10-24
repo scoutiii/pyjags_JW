4379bb2d65e876d6d87effc7efa86b496ba12043
Scout Jarman <scoutjarman@yahoo.com>
2025-10-24 16:22:20 -0600

    Major updates: using cibuildwheel to try to optimize the steps of building wheels for different distros and stuff

---
+++ ci/install_jags_linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+PREFIX="/opt/jags"
+mkdir -p /tmp/jags && cd /tmp/jags
+
+# always fetch the latest release automatically
+LATEST_URL=$(curl -Ls -o /dev/null -w '%{url_effective}' \
+  https://sourceforge.net/projects/mcmc-jags/files/JAGS/latest/download)
+curl -L -o jags.tar.gz "$LATEST_URL"
+
+tar -xf jags.tar.gz
+cd JAGS-*
+
+./configure --prefix="${PREFIX}" --disable-static
+make -j"$(nproc)"
+make install
+
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+echo "PKG_CONFIG_PATH=${PKG_CONFIG_PATH}"
---
+++ ci/install_jags_macos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+# Use brew to get JAGS (shared libs), then point pkg-config at it
+brew update
+brew install jags pkg-config
+echo "BREW_PREFIX=$(brew --prefix)"
+export PKG_CONFIG_PATH="$(brew --prefix)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+echo "PKG_CONFIG_PATH=${PKG_CONFIG_PATH}"
