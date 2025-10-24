a7e8f2f497217071b1eb84d1c3075ace2c303a1f
Scout Jarman <scoutjarman@yahoo.com>
2025-10-23 19:37:42 -0600

    Added new pyproject.toml and .github stuff, along with edits to io.py and __init__, according to chatGPT's suggestions to update this to install easier

--- pyjags/__init__.py
+++ pyjags/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2015-2016 Tomasz Miasko
 #               2020 Michael Nowotny
+#               2025 Scout Jarman and contributors
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -10,30 +11,118 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-from .incremental_sampling import (
+"""
+pyjags: Python bindings for JAGS
+
+This package re-exports convenience functions and classes at the top level.
+To improve robustness, heavy / compiled bits are **lazily imported** so that
+`import pyjags` succeeds even if optional system/runtime pieces are not present.
+The compiled extension is only loaded when you access e.g. `pyjags.Model`.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+# --- Version ---------------------------------------------------------------
+
+try:
+    from ._version import get_versions  # type: ignore
+    __version__ = get_versions()["version"]
+    del get_versions
+except Exception:  # pragma: no cover
+    __version__ = "0+unknown"
+
+# --- Eager, pure-Python re-exports (safe to import immediately) -----------
+
+from .incremental_sampling import (  # noqa: F401
     EffectiveSampleSizeCriterion,
     RHatDeviationCriterion,
     EffectiveSampleSizeAndRHatCriterion,
-    sample_until
+    sample_until,
 )
 
-# from .arviz import from_pyjags
-from .chain_utilities import (
+from .chain_utilities import (  # noqa: F401
     discard_burn_in_samples,
     extract_final_iteration_from_samples_for_initialization,
     merge_parallel_chains,
-    merge_consecutive_chains
+    merge_consecutive_chains,
 )
 
-from .dic import dic_samples
-from .io import (
+from .dic import dic_samples  # noqa: F401
+
+# io.py is safe to import even without deepdish; it raises only when used.
+from .io import (  # noqa: F401
     load_samples_dictionary_from_file,
-    save_samples_dictionary_to_file
+    save_samples_dictionary_to_file,
 )
 
-from .model import *
-from .modules import *
-from ._version import get_versions
+# --- Public API surface ----------------------------------------------------
 
-__version__ = get_versions()['version']
-del get_versions
+__all__ = [
+    # version
+    "__version__",
+    # incremental_sampling
+    "EffectiveSampleSizeCriterion",
+    "RHatDeviationCriterion",
+    "EffectiveSampleSizeAndRHatCriterion",
+    "sample_until",
+    # chain utilities
+    "discard_burn_in_samples",
+    "extract_final_iteration_from_samples_for_initialization",
+    "merge_parallel_chains",
+    "merge_consecutive_chains",
+    # DIC
+    "dic_samples",
+    # I/O helpers
+    "load_samples_dictionary_from_file",
+    "save_samples_dictionary_to_file",
+    # model & modules (lazily provided)
+    "Model",
+    # anything else exported by modules.py will be handled lazily
+]
+
+# --- Lazy import machinery (PEP 562) --------------------------------------
+
+def __getattr__(name: str):
+    """
+    Lazily import attributes from submodules to avoid importing the compiled
+    extension (pyjags.console via pyjags.model) until actually needed.
+    """
+    # Try model first (provides Model, etc.)
+    if name in {"Model"}:
+        from . import model as _model  # local import
+        try:
+            return getattr(_model, name)
+        except AttributeError:  # pragma: no cover
+            raise
+
+    # Fallback: check modules.py for any names users might grab from top-level
+    try:
+        from . import modules as _modules  # local import
+        if hasattr(_modules, name):
+            return getattr(_modules, name)
+    except Exception:
+        # If modules import fails, continue to final error below.
+        pass
+
+    # Last chance: load model/modules and see if name appeared in their __all__
+    for modname in (".model", ".modules"):
+        try:
+            mod = __import__(__name__ + modname, fromlist=["*"])
+            if hasattr(mod, name):
+                return getattr(mod, name)
+        except Exception:
+            # Ignore and try the next candidate
+            continue
+
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+# --- Type checking friendliness -------------------------------------------
+
+if TYPE_CHECKING:
+    # During static analysis, make the symbols visible without executing imports.
+    from .model import Model  # noqa: F401
+    # If you have specific exports in modules.py you want visible to type checkers,
+    # you can list them explicitly here as needed.
--- setup.py
+++ setup.py
@@ -1,150 +1,65 @@
-# encoding: utf-8
-#
-# Copyright (C) 2015-2016 Tomasz Miasko
-#               2020 Michael Nowotny
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import os, subprocess, sys
+from pybind11.setup_helpers import Pybind11Extension, build_ext as pybind_build_ext
+import numpy as np
 
-import io
-import os
-import platform
-# from distutils.core import setup
-# from distutils.extension import Extension
-from setuptools import dist, setup, Extension
-# from Cython.Distutils import build_ext
-import subprocess
-import sys
-import versioneer
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-DESCRIPTION = 'PyJAGS: The Python Interface to JAGS'
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-
-def add_pkg_config(ext, package):
-    flags_map = {
-        '-I': ['include_dirs'],
-        '-L': ['library_dirs', 'runtime_library_dirs'],
-        '-l': ['libraries'],
-    }
-
+def jags_pkg_config():
+    cflags, libs = [], []
     try:
-        args = ['pkg-config', '--libs', '--cflags', package]
-        output = subprocess.check_output(args)
-        output = output.decode()
-        for flag in output.split():
-            for attr in flags_map[flag[:2]]:
-                getattr(ext, attr).append(flag[2:])
+        cflags = subprocess.check_output(["pkg-config", "--cflags", "jags"], text=True).strip().split()
+        libs = subprocess.check_output(["pkg-config", "--libs", "jags"], text=True).strip().split()
+    except Exception:
+        pass
+    return cflags, libs
 
-        args = ['pkg-config', '--modversion', package]
-        output = subprocess.check_output(args, universal_newlines=True)
-        return output.strip()
-    except Exception as err:
-        print("Error while executing pkg-config: {}".format(err))
-        sys.exit(1)
+class BuildExt(pybind_build_ext):
+    def build_extensions(self):
+        cflags, libs = jags_pkg_config()
 
+        # Fallbacks if pkg-config not present or returns nothing
+        jags_dir = os.environ.get("JAGS_DIR", "")
+        if not cflags and jags_dir:
+            cflags += [f"-I{os.path.join(jags_dir, 'include')}"]
+        if not libs and jags_dir:
+            libs += [f"-L{os.path.join(jags_dir, 'lib')}", "-ljags"]
 
-def add_jags(ext):
-    if 'windows' in platform.system().lower():
-        JAGS_HOME = os.environ.get('JAGS_HOME')
+        # Common conda/homebrew hints (best-effort)
+        common_inc = ["/usr/include", "/usr/local/include", "/opt/homebrew/include", "/opt/conda/include"]
+        common_lib = ["/usr/lib", "/usr/local/lib", "/opt/homebrew/lib", "/opt/conda/lib"]
+        if not any("jags" in " ".join(libs) for libs in [libs]):
+            libs += [f"-L{p}" for p in common_lib] + ["-ljags"]
 
-        if JAGS_HOME is None:
-            JAGS_HOME = r'C:\Program Files\JAGS\JAGS-4.3.0'
-            print(f'environment variable JAGS_HOME not defined, defaulting to {JAGS_HOME}')
-        else:
-            print(f'environment variable JAGS_HOME is defined as {JAGS_HOME}')
-        version = os.path.split(JAGS_HOME)[1][5:]
-        version = '"{}"'.format(version)
+        for ext in self.extensions:
+            ext.include_dirs += [np.get_include()]
+            ext.extra_compile_args += ["-std=gnu++14", "-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"]
+            ext.extra_compile_args += [flag for flag in cflags if flag.startswith("-I")]
+            ext.extra_link_args += libs
 
-        ext.include_dirs.append(rf'{JAGS_HOME}\include')
-        if any('64' in element for element in platform.architecture()):
-            print('On 64 Bit Architecture')
-            lib_dir = rf'{JAGS_HOME}\x64\bin'
-        else:
-            print('On 32 Bit Architecture')
-            lib_dir = rf'{JAGS_HOME}\i386\bin'
+        # Check we have -ljags somewhere; fail early with a helpful message
+        if not any(arg == "-ljags" or arg.endswith("/libjags.so") for arg in sum([ext.extra_link_args for ext in self.extensions], [])):
+            raise RuntimeError(
+                "Could not find libjags. Install it first:\n"
+                "  Debian/Ubuntu: sudo apt install jags libjags-dev\n"
+                "  Homebrew:      brew install jags\n"
+                "  Conda:         conda install -c conda-forge jags\n"
+                "Or set JAGS_DIR to your JAGS prefix."
+            )
 
-        ext.library_dirs.append(lib_dir)
+        super().build_extensions()
 
-        # the Extension property runtime_library_dirs is not used on Windows
-        # ext.runtime_library_dirs.append(lib_dir)
+ext_modules = [
+    Pybind11Extension(
+        "pyjags.console",
+        sources=["pyjags/console.cc"],
+        include_dirs=[],
+        extra_compile_args=[],
+        extra_link_args=[],
+        language="c++",
+    )
+]
 
-        # ToDo: How to tell the Python build system to tell the C++ linker to look for a .dll instead of a .lib?
-        # ext.libraries.append('libjags')
-        ext.define_macros.append(('PYJAGS_JAGS_VERSION', version))
-    else:
-        version = add_pkg_config(ext, 'jags')
-        version = '"{}"'.format(version)
-        ext.define_macros.append(('PYJAGS_JAGS_VERSION', version))
-
-
-def add_numpy(ext):
-    try:
-        import numpy
-    except ImportError:
-        sys.exit('Please install numpy first.')
-    ext.include_dirs.append(numpy.get_include())
-
-
-def add_pybind11(ext):
-    ext.include_dirs.append('pybind11/include')
-    if not 'windows' in platform.system().lower():
-        ext.extra_compile_args.append('-std=c++14')
-
-
-if __name__ == '__main__':
-    ext = Extension('pyjags.console',
-                    language='c++',
-                    sources=['pyjags/console.cc'],
-                    # libraries=['libjags-4'],
-                    # library_dirs=['.']
-                    )
-
-    add_jags(ext)
-    add_numpy(ext)
-    add_pybind11(ext)
-    setup(name='pyjags',
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          # cmdclass={'build_ext': build_ext},
-          description='Python interface to JAGS library for Bayesian data analysis.',
-          long_description=long_description,
-          long_description_content_type='text/markdown',
-          author=u'Michael Nowotny',
-          author_email='nowotnym@gmail.com',
-          url='https://github.com/michaelnowotny/pyjags',
-          license='GPLv2',
-          classifiers=[
-              'Development Status :: 4 - Beta',
-              'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-              'Operating System :: POSIX',
-              'Programming Language :: C++',
-              'Programming Language :: Python :: 3.6',
-              'Programming Language :: Python :: 3.7',
-              'Programming Language :: Python :: 3.8',
-              'Programming Language :: Python :: 3.9',
-              'Programming Language :: Python',
-              'Topic :: Scientific/Engineering :: Information Analysis',
-              'Topic :: Scientific/Engineering',
-          ],
-          packages=['pyjags'],
-          ext_modules=[ext],
-          install_requires=[
-              'numpy',
-              'arviz',
-              'deepdish'
-          ],
-          test_suite='test',
-          )
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExt},
+)
